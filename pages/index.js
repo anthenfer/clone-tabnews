bd8045ed942029815fe7c96f51b6140761ef985a
Antonio <antoniohenriqueferreira@outlook.com.br>
2025-10-02 21:13:59 -0300

    Adicionando os scripts `lint:check` e `lint:fix`

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,6 @@
 function Home() {
-    return <h1>César, eu amo você! Se você me ama, dá uma risadinha :)</h1>
+  return <h1>César, eu amo você! Se você me ama, dá uma risadinha :)</h1>;
 }
 
 export default Home;
+
