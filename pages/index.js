5611935e6e69656fbe288c112cee9673a983f7be
Antonio Henrique <antoniohenriqueferreira@outlook.com.br>
2025-10-20 23:33:52 -0300

    Adicionando pacote de testes automatizados JEST

--- pages/index.js
+++ pages/index.js
@@ -1,4 +1,4 @@
 function Home() {
-  return <h1>Essa frase dará lugar para várias lindas histórias :)</h1>;
+  return <h1>O que precisa ser feito aqui: construir um belo projeto! O que precisa ser protegido? </h1>;
 }
 export default Home;
