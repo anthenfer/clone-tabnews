68398cc19772c627835cec60684ebccdd5a01b6b
Antonio H. F. Aurelano <109366775+anthenfer@users.noreply.github.com>
2026-01-17 23:04:27 +0000

    Change the home text

--- pages/index.js
+++ pages/index.js
@@ -3,10 +3,8 @@ function Home() {
     Projeto em andamento.
     Uma linda homenagem aqui deverá ocorrer.
     E posteriormente, não só uma homenagem minha, mas, a de várias outras pessoas.
-    É o meu amor transformado em algo fisíco s2.
-    <br>
-    Físico seria a palavra? 
-    </br>
+    É o meu amor transformado em algo fisíco s2. Físico seria a palavra? 
+    
   </p>;
 }
 export default Home;
